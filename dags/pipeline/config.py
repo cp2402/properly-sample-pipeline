2c7326e60d319178437a38d13be111f784da155a
Christopher Pang <hello@christopherpang.ca>
2024-03-18 22:26:50 -0400

    fix: update postal code file name

--- dags/pipeline/config.py
+++ dags/pipeline/config.py
@@ -3,7 +3,7 @@ params = {
     "DB_ENGINE": "postgresql+psycopg2://airflow:airflow@postgres/airflow",
     "DB_SCHEMA": "public",
     "POSTAL_LOCATIONS_URL": "https://www.serviceobjects.com/public/zipcode/ZipCodeFiles.zip",
-    "POSTAL_LOCATIONS_FILE_NAME": "CanadianPostalCodes202112.csv",
+    "POSTAL_LOCATIONS_FILE_NAME": "CanadianPostalCodes202312.csv",
     "POSTAL_LOCATIONS_FILE_TAG": "postal-locations",
     "PROPERTY_SALES_URL": "https://toronto-property-sales-kaggle.s3.us-west-2.amazonaws.com/properties.zip",
     "PROPERTY_SALES_FILE_NAME": "properties.csv",
